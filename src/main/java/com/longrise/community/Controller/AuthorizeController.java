0cf992d5781faba2259b812a4e03fd0ab678f474
QQ-JiangWang <1120454016@qq.com>
2019-11-03 19:20:30 +0800

    新增登陆

--- src/main/java/com/longrise/community/Controller/AuthorizeController.java
+++ src/main/java/com/longrise/community/Controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.longrise.community.Controller;
 
 import com.longrise.community.dto.AccessTokenDTO;
 import com.longrise.community.dto.GithubUser;
+import com.longrise.community.mapper.UserMapper;
+import com.longrise.community.model.User;
 import com.longrise.community.provider.GihubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -20,9 +25,12 @@ public class AuthorizeController {
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -31,7 +39,21 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = gihubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = gihubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if (user != null){
+            User user1 = new User();
+            user1.setToken(UUID.randomUUID().toString());
+            user1.setName(user.getName());
+            user1.setAccountId(String.valueOf(user.getId()));
+            user1.setGmtCreate(System.currentTimeMillis());
+            user1.setGmtModified(user1.getGmtCreate());
+            userMapper.insert(user1);
+            //登陆成功，写cookie
+            request.getSession().setAttribute("user",user);
+
+            return "redirect:/";
+        }else{
+            //登陆失败
+            return "redirect:/";
+        }
     }
 }
