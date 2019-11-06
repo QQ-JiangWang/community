cbeaac1c6786ae9550f1feac71f567f136a9a762
QQ-JiangWang <1120454016@qq.com>
2019-11-06 20:50:41 +0800

    新增表单

--- src/main/java/com/longrise/community/Controller/AuthorizeController.java
+++ src/main/java/com/longrise/community/Controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -30,7 +32,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -41,14 +44,16 @@ public class AuthorizeController {
         GithubUser user = gihubProvider.getUser(accessToken);
         if (user != null){
             User user1 = new User();
-            user1.setToken(UUID.randomUUID().toString());
+            String s = UUID.randomUUID().toString();
+            user1.setToken(s);
             user1.setName(user.getName());
             user1.setAccountId(String.valueOf(user.getId()));
             user1.setGmtCreate(System.currentTimeMillis());
             user1.setGmtModified(user1.getGmtCreate());
             userMapper.insert(user1);
             //登陆成功，写cookie
-            request.getSession().setAttribute("user",user);
+            response.addCookie(new Cookie("token",s));
+            //request.getSession().setAttribute("user",user);
 
             return "redirect:/";
         }else{
--- src/main/java/com/longrise/community/Controller/IndexController.java
+++ src/main/java/com/longrise/community/Controller/IndexController.java
@@ -1,15 +1,34 @@
 package com.longrise.community.Controller;
 
+import com.longrise.community.mapper.UserMapper;
+import com.longrise.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class IndexController {
-
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public  String hello(){
+    public  String hello(HttpServletRequest request){
+      Cookie[] cookies = request.getCookies();
+      for(Cookie cookie:cookies){
+        if("token".equals(cookie.getName())){
+          String token = cookie.getValue();
+          User user = userMapper.findByToken(token);
+          if(user != null){
+            request.getSession().setAttribute("user",user);
+          }
+          break;
+        }
+      }
 
         return "index";
     }
