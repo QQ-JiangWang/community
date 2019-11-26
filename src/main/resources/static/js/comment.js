65dfed6c2f36c7b8509c070b85885c437ac4839c
QQ-JiangWang <1120454016@qq.com>
2019-11-26 20:57:48 +0800

    新增回复功能

---
+++ src/main/resources/static/js/comment.js
@@ -0,0 +1,45 @@
+/**
+ * 提交回复
+ */
+function post() {
+    debugger
+    var questionId = $("#question_id").val();
+    var content = $("#comment_content").val();
+    comment2target(questionId, 1, content);
+}
+
+function comment2target(targetId, type, content) {
+    if (!content) {
+        alert("不能回复空内容~~~");
+        return;
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": targetId,
+            "content": content,
+            "type": type
+        }),
+        success: function (response) {
+            debugger
+            console.log(response);
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=2f2b36c135ac358c50e9&redirect_uri=" + document.location.origin + "/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
+        },
+        dataType: "json"
+    });
+}
